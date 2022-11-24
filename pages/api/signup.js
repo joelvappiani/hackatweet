e66fec27b05867129535b8f024bba229bf599634
Angélique <angelique.massolo@gmail.com>
2022-11-24 13:07:55 +0100

    routes signin/signup

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,7 +1,10 @@
 const User = require("../../models/User");
 require("../../models/connection");
 const { checkBody } = require("../../modules/checkbody");
+
 const bcrypt = require("bcrypt");
+
+const uid2 = require("uid2");
 
 export default function signup(req, res) {
   if (req.method === "POST") {
@@ -10,11 +13,24 @@ export default function signup(req, res) {
       return;
     }
 
+    // Check if the user has not already been registered
     User.findOne({ username: req.body.username }).then((data) => {
-      if (data && bcrypt.compareSync(req.body.password, data.password)) {
-        res.json({ result: true, token: data.token });
+      if (data === null) {
+        const hash = bcrypt.hashSync(req.body.password, 10);
+
+        const newUser = new User({
+          token: uid2(32),
+          username: req.body.username,
+          firsName: req.body.firstName,
+          password: hash,
+        });
+
+        newUser.save().then((newDoc) => {
+          res.json({ result: true, token: newDoc.token });
+        });
       } else {
-        res.json({ result: false, error: "User not found or wrong password" });
+        // User already exists in database
+        res.json({ result: false, error: "User already exists" });
       }
     });
 
