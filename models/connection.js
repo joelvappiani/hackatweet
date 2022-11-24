37196f3816d5429e8a29963c7982327b22f4c297
Angélique <angelique.massolo@gmail.com>
2022-11-24 12:26:47 +0100

    route signin

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectionString = 'mongodb+srv://djovap:djovap@cluster0.watf3oo.mongodb.net/hackatweet';
+const connectionString =
+  "mongodb+srv://djovap:djovap@cluster0.watf3oo.mongodb.net/hackatweet";
 
-mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
-  .then(() => console.log('Database connected'))
-  .catch(error => console.error(error));
+mongoose
+  .connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log("Database connected"))
+  .catch((error) => console.error(error));
