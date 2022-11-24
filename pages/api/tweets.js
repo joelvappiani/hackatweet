4dd81f5b8affc7bf257a1420831f9f8e4a8338c1
Angélique <angelique.massolo@gmail.com>
2022-11-24 14:55:32 +0100

    setup front

--- pages/api/tweets.js
+++ pages/api/tweets.js
@@ -1,30 +1,32 @@
 require("../../models/connection");
-const Tweet = require('../../models/Tweet')
-const User = require('../../models/User')
+const Tweet = require("../../models/Tweet");
+const User = require("../../models/User");
 export default async (req, res) => {
-    if (req.method === 'POST') {
-      const {user, message, nbLikes} = req.body
-      const date = new Date()
-      const newTweet = await new Tweet({user, message, nbLikes, date})
-      newTweet.save()
-      console.log(newTweet)
-      res.json({result: true})
-    } 
-    if (req.method === 'GET') {
-      const allTweets = await Tweet.find().populate({ path: 'user', select: 'username' })
-      
-      
-      res.json({
-        result: true,
-        tweets: allTweets
-      })
-    }
-    if (req.method === 'DELETE') {
-      res.json({method: 'delete'})
-    }
+  if (req.method === "POST") {
+    const { user, message, nbLikes } = req.body;
+    const date = new Date();
+    const newTweet = await new Tweet({ user, message, nbLikes, date });
+    newTweet.save();
+    console.log(newTweet);
+    res.json({ result: true });
   }
-  // .populate({
-  //   path: 'copies.loaned_to',
-  //   select:
-  //     'username',
-  // })
+  if (req.method === "GET") {
+    const allTweets = await Tweet.find().populate({
+      path: "user",
+      select: "username",
+    });
+
+    res.json({
+      result: true,
+      tweets: allTweets,
+    });
+  }
+  if (req.method === "DELETE") {
+    res.json({ method: "delete" });
+  }
+};
+// .populate({
+//   path: 'copies.loaned_to',
+//   select:
+//     'username',
+// })
