358e8eed2cacee7495a84fea6ca8506af2147337
joelvappiani <joel.vappiani@gmail.com>
2022-11-25 17:29:27 +0100

    first commit

--- components/Search.js
+++ components/Search.js
@@ -1,14 +1,18 @@
 import styles from "../styles/Hashtag.module.css";
 import { useState } from "react";
+import Link from "next/link";
 
 
 const Search = (props) => {
     const [search, setSearch] = useState()
     console.log(search)
+    const handleClick = () => {
+
+    }
     return (
         <div className={styles.search}>
             <input className={styles.inputField} placeholder={`#${props.hashtag}`} onChange={(e)=> setSearch(e.target.value)}value={search}/>
-            <button className={styles.searchBtn} >Search</button>
+            <Link href={`/post/${search}`}><button className={styles.searchBtn}>Search</button></Link>
         </div>
     )
 }
